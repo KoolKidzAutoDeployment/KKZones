8ed1c868ebd78022529d30e0abe11f583913525d
Rodney_RWR <rodney.rwr.yt@gmail.com>
2023-11-10 08:24:47 +1100

    Push

---
+++ src/main/java/com/koolkidzmc/kkzones/KKZones.java
@@ -0,0 +1,17 @@
+package com.koolkidzmc.kkzones;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KKZones extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
